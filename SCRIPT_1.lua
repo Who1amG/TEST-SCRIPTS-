939aebcf71bd6e7029cd797d3608827a16ac5f67
WHOAMI <212384818+Who1amG@users.noreply.github.com>
2026-02-09 08:34:22 -0700

    Initial commit of SCRIPT_1

---
+++ SCRIPT_1.lua
@@ -0,0 +1,18 @@
+-- SCRIPT_1 content here
+-- Features: ESP, Animations, UI Theming
+
+local function setupESP()
+    -- Code for ESP functionality
+end
+
+local function playAnimation()
+    -- Code for animations
+end
+
+local function setupUI()
+    -- Code for UI theming
+end
+
+setupESP()
+playAnimation()
+setupUI()
